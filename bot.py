ac4e8731ec21b0c04489aafea22dece77c79ec68
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2021-02-18 16:35:14 -0600

    tdameritrade api test

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
 import requests
+from config import consumer_key
 
-print('hello')
+stock = 'GOOG'
+endpoint = r'https://api.tdameritrade.com/v1/marketdata/{}/pricehistory'.format(stock)
+
+payload = {'apikey': consumer_key,
+                    'periodType': 'day',
+                    'frequencyType': 'minute',
+                    'frequency': '30',
+                    'period': '1',
+                    'endDate': None,
+                    'startDate': None,
+                    'needExtendedHoursData': 'false'}
+
+content = requests.get(url = endpoint, params = payload)
+
+data = content.json()
+print(data)
