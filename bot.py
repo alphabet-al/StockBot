ce6730d15f7bae70f1eb0dc10f98877d273f3636
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2022-07-27 22:26:21 -0500

    sync commit

--- bot.py
+++ bot.py
@@ -4,14 +4,16 @@ from config import consumer_key
 stock = 'GOOG'
 endpoint = r'https://api.tdameritrade.com/v1/marketdata/{}/pricehistory'.format(stock)
 
-payload = {'apikey': consumer_key,
-                    'periodType': 'day',
-                    'frequencyType': 'minute',
-                    'frequency': '30',
-                    'period': '1',
-                    'endDate': None,
-                    'startDate': None,
-                    'needExtendedHoursData': 'false'}
+payload = {
+                'apikey': consumer_key,
+                'periodType': 'day',
+                'frequencyType': 'minute',
+                'frequency': '30',
+                'period': '1',
+                'endDate': None,
+                'startDate': None,
+                'needExtendedHoursData': 'false'
+            }
 
 content = requests.get(url = endpoint, params = payload)
 
